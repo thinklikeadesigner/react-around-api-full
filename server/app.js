1582e9a6b501f3921d7481834cf294f1b7b4f539
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-05-11 22:56:11 -0500

    app use error next

--- server/app.js
+++ server/app.js
@@ -18,6 +18,19 @@ app.use('/', (req, res) => {
   res.status(404).send({ message: 'Requested resource not found' });
 });
 
+app.use((err, req, res, next) => {
+  // if an error has no status, display 500
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      // check the status and display a message based on it
+      message: statusCode === 500
+        ? 'An error occurred on the server'
+        : message,
+    });
+});
+
 mongoose.connect('mongodb://localhost:27017/aroundb', {
   useNewUrlParser: true,
   useCreateIndex: true,
