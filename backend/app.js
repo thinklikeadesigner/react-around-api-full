ee797b6b3db34f01df73b3cc1a9b20475479298a
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-11-02 16:09:26 -0500

    prod port

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
-const port = process.env.PORT || 3000
+const port = 16856 || 3000
 
 app.use(helmet());
 app.use(express.json());
